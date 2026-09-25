5a6ce4a54ced5dbd127a682df44bbb9077f10661
Zakaria Elguazzar <152805085+ZakariaElguazzar@users.noreply.github.com>
2026-09-25 19:45:59 +0100

    Time: 191 ms (54.32%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-SELECT * FROM users WHERE email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+[.]com$' ORDER BY user_id;
+-- Write your PostgreSQL query statement below
+SELECT * FROM users WHERE email ~ '^[A-Za-z0-9_]+@[A-Za-z]+[.]com$' ORDER BY user_id;
